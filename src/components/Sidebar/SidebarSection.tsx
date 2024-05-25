29b8604a09b4521642eae727a4651dc6b97b44e3
Abdul Wahab <abdulwabby@gmail.com>
2024-05-25 19:58:29 +0500

    fixed_ts_issue

--- src/components/Sidebar/SidebarSection.tsx
+++ src/components/Sidebar/SidebarSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SidebarCard from "../ui/SidebarCard";
+import SidebarCard from "./SidebarCard";
 import { PROFILE_SIDEBAR } from "../../constants/constant";
 
 const Sidebar = () => {
@@ -7,11 +7,9 @@ const Sidebar = () => {
     <section className="flex flex-col min-w-[250px] max-w-[250px] bg-[#181849] min-h-screen">
       <h2 className="text-white text-2xl font-bold mt-4 ml-6">Profile</h2>
       <div className="flex flex-col w-full mt-5">
-        {PROFILE_SIDEBAR.map(
-          (sidebar: { id: React.Key | null | undefined; title: string }) => (
-            <SidebarCard key={sidebar.id} title={sidebar.title} />
-          )
-        )}
+        {PROFILE_SIDEBAR.map((sidebar) => (
+          <SidebarCard key={sidebar.id} title={sidebar.title} />
+        ))}
       </div>
     </section>
   );
