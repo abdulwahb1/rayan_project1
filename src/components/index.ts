c0309afe90e98bc8284f460c73233a9e2986bbcc
Abdul Wahab <abdulwabby@gmail.com>
2024-05-27 01:34:51 +0500

    added_required_files

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,13 @@ import { SelectTopics } from "./SelectTopics/SelectTopics";
 import QuickLinks from "./QuickLinks/QuickLinkSection";
 import Sidebar from "./Sidebar/SidebarSection";
 import ProfileQuickLinkSection from "./ProfileQuickLink/ProfileQuickLinkSection";
+import MeetingDialog from "./Meeting/MeetingDialog";
 
-export { Navbar, SelectTopics, QuickLinks, Sidebar, ProfileQuickLinkSection };
+export {
+  Navbar,
+  SelectTopics,
+  QuickLinks,
+  Sidebar,
+  ProfileQuickLinkSection,
+  MeetingDialog,
+};
