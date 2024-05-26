eb1ee918a5a8fd6796ab178e5ca4a632013dfff3
Abdul Wahab <abdulwabby@gmail.com>
2024-05-26 22:30:28 +0500

    created_routes_for_student/teacher

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,41 +1,44 @@
 import React from "react";
-import { QuickLinks, SelectTopics } from "@/components";
+import { Navbar, QuickLinks, SelectTopics } from "@/components";
 
 const Search = () => {
   return (
-    <section className="w-full h-screen flex flex-col justify-start items-center mt-48">
-      <div className="max-w-[25.5rem] flex flex-col justify-center items-center">
-        <h1 className="text-darkblueui font-bold text-3xl">
-          What do you want to learn?
-        </h1>
-        <p className="text-darkblueui font-normal text-sm text-center">
-          Search topics, teachers and price ranges about anything you want to
-          learn.
-        </p>
-      </div>
-      <div className="border-2 border-[#d7e3f4] rounded-full w-96 h-12 mt-8">
-        <form className="w-full max-w-sm">
-          <div className="flex items-center py-2">
-            <input
-              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              placeholder=""
-              aria-label="Full name"
-            />
-            <button
-              className="h-[2rem] w-[5rem] flex-shrink-0 bg-darkblueui text-sm text-white py-1 px-2 rounded-full mr-1 hover:bg-blueui"
-              type="button"
-            >
-              Search
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="flex mt-4">
-        <SelectTopics />
-      </div>
-      <div className="mt-4 flex flex-col min-h-[100px]">
-        <QuickLinks />
+    <section className="w-full h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-col justify-center items-center mt-auto mb-auto">
+        <div className="flex flex-col justify-center items-center mt-20">
+          <h1 className="text-darkblueui font-bold text-3xl">
+            What do you want to learn?
+          </h1>
+          <p className="text-darkblueui font-normal text-sm text-center">
+            Search topics, teachers and price ranges about anything you <br />{" "}
+            want to learn.
+          </p>
+        </div>
+        <div className="border-2 border-[#d7e3f4] rounded-full w-96 h-12 mt-8 mx-auto">
+          <form className="w-full max-w-sm">
+            <div className="flex items-center py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder=""
+                aria-label="Full name"
+              />
+              <button
+                className="h-[2rem] w-[5rem] flex-shrink-0 bg-darkblueui text-sm text-white py-1 px-2 rounded-full mr-1 hover:bg-blueui"
+                type="button"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="flex mt-4 justify-center">
+          <SelectTopics />
+        </div>
+        <div className="mt-4 flex flex-col min-h-[100px] mx-auto">
+          <QuickLinks />
+        </div>
       </div>
     </section>
   );
---
+++ src/pages/create/teacher/profilesetup.tsx
@@ -0,0 +1,19 @@
+import Stepper from "@/components/Stepper/Stepper";
+import React from "react";
+
+const profilesetup = () => {
+  return (
+    <section className="flex flex-col w-full min-h-screen    justify-center items-center">
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <h1 className="text-darkblueui text-lg">Setting up your Profile</h1>
+        <span className="text-blueui text-base">
+          To make the platform as engaging as possible, we require some <br />
+          information so our teachers know who they are talking to.
+        </span>
+        <Stepper />
+      </div>
+    </section>
+  );
+};
+
+export default profilesetup;
