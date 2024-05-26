3eb2321bb8018f542d8b9888a88b01ca7c3dce0a
Abdul Wahab <abdulwabby@gmail.com>
2024-05-26 21:44:09 +0500

    navbar_changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,19 +6,31 @@ import { FaUser } from "react-icons/fa6";
 import { BiConversation } from "react-icons/bi";
 import { FaRegCircleQuestion } from "react-icons/fa6";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  const isActive = (href: string) => router.pathname === href;
   return (
     <div className="flex w-full min-h-18 mt-2 justify-between shadow ">
       <div className="flex min-w-8  bg-white"> </div>
       <div className="flex min-w-[380px] max-h-20 ">
         {/* First Icon */}
-        <div className="flex flex-col w-16 h-16 bg-white hover:bg-blue-500 rounded-full hover:text-white outerdivclass">
+        <div
+          className={`flex flex-col w-16 h-16 hover:bg-blue-500 rounded-full hover:text-white outerdivclass ${
+            isActive("/Search") ? "bg-blue-500 text-white" : ""
+          }`}
+        >
           <div className="ml-2 mt-0">
-            <div className="ml-2 text-3xl mt-1 hover:fill-white">
-              <IoIosSearch className="search-icon" />
+            <div
+              className={`ml-2 text-3xl mt-1 hover:fill-white ${
+                isActive("/Search") ? "search-icon-active" : "search-icon"
+              }`}
+            >
+              <IoIosSearch />
             </div>
-            <div className="text-sm ml-1">
+            <div className="text-sm ml-0.5">
               <Link href="/Search">Search</Link>
             </div>
           </div>
@@ -29,7 +41,7 @@ const Navbar = () => {
             <div className="ml-3 text-2xl mt-1 hover:fill-white">
               <SlBookOpen className="search-icon" />
             </div>
-            <div className="text-sm ml-1 mt-1">
+            <div className="text-sm ml-0.5 mt-1">
               <Link href="/">Library</Link>
             </div>
           </div>
@@ -51,7 +63,7 @@ const Navbar = () => {
             <div className="ml-2 text-3xl mt-1 hover:fill-white">
               <BiConversation className="search-icon" />
             </div>
-            <div className="text-sm ml-3">
+            <div className="text-sm ml-2">
               <Link href="/">Chat</Link>
             </div>
           </div>
