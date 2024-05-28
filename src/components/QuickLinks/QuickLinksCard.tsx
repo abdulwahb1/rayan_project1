67d0138213eccfc0af15d1ad18559192609a2355
Abdul Wahab <abdulwabby@gmail.com>
2024-05-29 02:02:49 +0500

    added_prop

--- src/components/QuickLinks/QuickLinksCard.tsx
+++ src/components/QuickLinks/QuickLinksCard.tsx
@@ -4,16 +4,25 @@ import Link from "next/link";
 type QuickLinksCardProps = {
   title: string;
   textSize?: "md" | "sm"; // Add a new prop for text size
+  svg?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>; // Add a new prop for SVG
+  marginLeft?: boolean; // Add a new prop for margin-left
 };
 
-const QuickLinksCard = ({ title, textSize = "md" }: QuickLinksCardProps) => {
+const QuickLinksCard = ({
+  title,
+  textSize = "md",
+  svg: Svg,
+  marginLeft = true, // Default to true
+}: QuickLinksCardProps) => {
   const textSizeClass = textSize === "sm" ? "text-sm" : "text-md"; // Determine the text size class based on the textSize prop
+  const marginLeftClass = marginLeft ? "ml-2" : ""; // Determine the margin-left class based on the marginLeft prop
 
   return (
     <div
-      className={`flex flex-col bg-[#d7e3f4] rounded-full ml-2 justify-center items-center mt-2 ${textSizeClass} text-darkblueui px-3 py-1`}
+      className={`flex bg-[#d7e3f4] rounded-full justify-center items-center mt-2 ${textSizeClass} text-darkblueui px-3 py-1 ${marginLeftClass}`}
     >
       <Link href="/">{title}</Link>
+      {Svg && <Svg />} {/* Render the SVG if it's provided */}
     </div>
   );
 };
