2af7e6d67cd69b37c905ce00b402622693f8304d
Abdul Wahab <abdulwabby@gmail.com>
2024-05-29 02:03:54 +0500

    added_icons

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,4 +1,6 @@
 import ArrowIconBlue from "./ArrowIconBlue";
 import ArrowIconWhite from "./ArrowIconWhite";
+import CrossSvg from "./CrossSvg";
+import LittleStar from "./LittleStar";
 
-export { ArrowIconBlue, ArrowIconWhite };
+export { ArrowIconBlue, ArrowIconWhite, CrossSvg, LittleStar };
